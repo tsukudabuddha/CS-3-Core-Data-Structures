de7358b83accb4ba25926ca00939164cdab9542c
tsukudabuddha <andrewtsukuda@gmail.com>
2018-01-21 19:17:43 -0800

    completed queue array

--- source/queue.py
+++ source/queue.py
@@ -83,30 +83,57 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        # Check if empty
+        if len(self.list) == 0:
+            return True
+
+        return False
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        # Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        # Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        # Return front item, if any
+
+        # Check if queue is empty
+        if self.is_empty():
+            return None
+
+        return self.list[0]
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        # Remove and return front item, if any
+
+        # Check if queue is empty
+        if self.is_empty():
+            raise ValueError("Queue is empty")
+
+        # Store front item data
+        front_item_data = self.list[0]
+
+        # Loop through items and overwrite
+        for i in range(0, len(self.list) - 1):
+            self.list[i] = self.list[i + 1]
+
+        # Remove end data
+        self.list.remove(self.list[len(self.list) - 1])
+        return front_item_data
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-Queue = LinkedQueue
-# Queue = ArrayQueue
+# Queue = LinkedQueue
+Queue = ArrayQueue
