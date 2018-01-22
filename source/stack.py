5abc82b8cb3b073bd893b996b1fb39a4732168a7
tsukudabuddha <andrewtsukuda@gmail.com>
2018-01-21 19:30:48 -0800

    completed stack linked list

--- source/stack.py
+++ source/stack.py
@@ -21,27 +21,48 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        # Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        # Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        # Push given item
+        self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        # Return top item, if any
+
+        # Check if empty
+        if self.list.is_empty():
+            return None
+
+        return self.list.head.data
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        # Remove and return top item, if any
+
+        # Check if empty
+        if self.list.is_empty():
+            raise ValueError("Stack is empty")
+
+        # Store data to return
+        top_item_data = self.list.head.data
+
+        # Remove top item from stack
+        self.list.delete(top_item_data)
+
+        return top_item_data
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
