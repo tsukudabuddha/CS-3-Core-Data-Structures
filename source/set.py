87f64813578f4562dadd89fdcf00ce8bdf91d9e6
tsukudabuddha <andrewtsukuda@gmail.com>
2018-01-29 17:19:51 -0800

    Use wrapper to report size property instead of tracking in set class

--- source/set.py
+++ source/set.py
@@ -8,7 +8,11 @@ class Set(object):
     def __init__(self, init_size=8):
         """Initialize this hash table with the given initial size."""
         self.ht = HashTable(init_size)
-        self.size = 0
+
+    @property
+    def size(self):
+        """Return size of ht."""
+        return self.ht.size
 
     def contains(self, element):
         """Check if the element is store in set (self)."""
@@ -17,12 +21,10 @@ class Set(object):
     def add(self, element):
         """Add new element to set, if unique."""
         self.ht.set(element, None)
-        self.size = self.ht.size
 
     def remove(self, element):
         """Remove element from set if present. Else raise keyerror."""
         self.ht.delete(element)  # Key error is raised in function
-        self.size = self.ht.size
 
     def union(self, other_set):
         """Return a new set that is the union of self and other_set."""
