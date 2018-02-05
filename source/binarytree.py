c7245c28ac5d9fae1264dba9a81d5b32b5a21390
tsukudabuddha <andrewtsukuda@gmail.com>
2018-02-05 11:11:13 -0800

    Completed traverse pre order recursive

--- source/binarytree.py
+++ source/binarytree.py
@@ -214,12 +214,14 @@ class BinarySearchTree(object):
         Start at the given node and visit each node with the given function.
         TODO: Running time: ??? Why and under what conditions?
         TODO: Memory usage: ??? Why and under what conditions?"""
-        # TODO: Visit this node's data with given function
-        ...
-        # TODO: Traverse left subtree, if it exists
-        ...
-        # TODO: Traverse right subtree, if it exists
-        ...
+        # Visit this node's data with given function
+        visit(node)
+        # Traverse left subtree, if it exists
+        if node.left is not None:
+            self._traverse_pre_order_recursive(node.left, visit)
+        # Traverse right subtree, if it exists
+        if node.right is not None:
+            self._traverse_pre_order_recursive(node.right, visit)
 
     def _traverse_pre_order_iterative(self, node, visit):
         """Traverse this binary tree with iterative pre-order traversal (DFS).
